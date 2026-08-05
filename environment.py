206a31ccdd47e2396865c4ae2bb70c9980bd6999
Luca <luca.fabbri98@outlook.it>
2026-08-05 19:17:05 +0200

    creata classe giocatore

---
+++ environment.py
@@ -0,0 +1,58 @@
+"""
+Definizione di ambiente e regole del simulatore
+"""
+
+from dataclasses import dataclass
+import pygame
+import pyyaml as yaml
+from pathlib import Path
+
+def parse_parameters(config_name: str) -> dict:
+    #Parsing parametri necessari. Ritorna un dizionario con dentro config
+    with open(config_name, "r") as f:
+        config = yaml.safe_load(f)
+
+    return config
+
+class Player:
+    """Definizione del giocatore"""
+
+    x: float
+    y: float
+    color: tuple[int, int, int]
+    width: int #px
+    height: int #px
+    speed: int #px/s
+
+    @property 
+    def rect(self) -> pygame.Rect:
+        return pygame.Rect(
+            round(self.x - self.width / 2),
+            round(self.y - self.height / 2),
+            self.width,
+            self.height,
+        )
+
+    def move(self, action: int, delta_time: float) -> None:
+        """Applica un'azione al giocatore. -1 Sinistra, 0 Fermo, +1 Destra
+            Si muove di x = v*t dove v = player speed e t il tempo di movimento
+        """
+        if action not in (-1, 0, 1):
+            raise ValueError(f"Azione non valida: {action}. Usa -1, 0 oppure 1.")
+
+        self.x += action * self.speed * delta_time
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.rect(screen, self.color, self.rect, border_radius=5)
+
+    def __init__(self, x, y, color, player_config):
+        """
+        Nel costruttore inizializzo i parametri propri del giocatore
+        """
+
+        self.width = player_config["width"]
+        self.height = player_config["height"]
+        self.speed = player_config["speed"]
+        self.x = x
+        self.y = y
+        self.color = color
